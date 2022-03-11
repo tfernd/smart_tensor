a095ce000c05d09c6b3780ccab26f13074d10e83
Thales Fernandes <ThalesFDFernandes@gmai.com>
2022-03-11 03:48:28 +0100

    add DIM to Tensor

--- src/tensor/mod.rs
+++ src/tensor/mod.rs
@@ -10,15 +10,17 @@ use crate::traits::{
 
 #[doc(hidden)]
 macro_rules! make_tensor {
-    ( $tensor:ident, [$($s:tt)+] ) => {
+    ( $tensor:ident, $dim:literal, [$($s:tt)+] ) => {
         /// A n-dimensional tensor.
         #[derive(Debug)]
         pub struct $tensor<D: DataTrait, $(const $s: usize),+> {
             pub(crate) data: D,
         }
 
-        impl<D: DataTrait, $(const $s: usize),+> TensorTrait for $tensor<D, $($s),+> {
+        impl<D: DataTrait, $(const $s: usize),+> TensorTrait<$dim> for $tensor<D, $($s),+> {
             type Data = D;
+
+            const SHAPE: [usize; $dim] = [$($s),+];
 
             #[inline]
             unsafe fn new(data: Self::Data) -> Self {
@@ -26,17 +28,17 @@ macro_rules! make_tensor {
             }
         }
 
-        impl<D: DataEmptyTrait, $(const $s: usize),+> TensorEmptyTrait for $tensor<D, $($s),+> {}
+        impl<D: DataEmptyTrait, $(const $s: usize),+> TensorEmptyTrait<$dim> for $tensor<D, $($s),+> {}
 
-        impl<D: DataFullTrait, $(const $s: usize),+> TensorFullTrait for $tensor<D, $($s),+>
+        impl<D: DataFullTrait, $(const $s: usize),+> TensorFullTrait<$dim> for $tensor<D, $($s),+>
         where
             D::Type: Copy + Zero + One, {}
 
     }
 }
 
-make_tensor!(Tensor1, [S0]);
-make_tensor!(Tensor2, [S0 S1]);
-make_tensor!(Tensor3, [S0 S1 S2]);
-make_tensor!(Tensor4, [S0 S1 S2 S3]);
-make_tensor!(Tensor5, [S0 S1 S2 S3 S4]);
+make_tensor!(Tensor1, 1, [S0]);
+make_tensor!(Tensor2, 2, [S0 S1]);
+make_tensor!(Tensor3, 3, [S0 S1 S2]);
+make_tensor!(Tensor4, 4, [S0 S1 S2 S3]);
+make_tensor!(Tensor5, 5, [S0 S1 S2 S3 S4]);
--- src/traits/tensor.rs
+++ src/traits/tensor.rs
@@ -3,19 +3,21 @@ use num_traits::{One, Zero};
 use super::{DataEmptyTrait, DataFullTrait, DataTrait};
 
 /// A marker for tensor.
-pub trait TensorTrait: Sized
+pub trait TensorTrait<const DIM: usize>: Sized
 where
     Self::Data: DataTrait,
 {
     /// The data structure type.
     type Data;
 
+    const SHAPE: [usize; DIM];
+
     /// Creates a new tensor from `data`.
     unsafe fn new(data: Self::Data) -> Self;
 }
 
 /// A marker to create a tensor with empty data.
-pub trait TensorEmptyTrait: TensorTrait
+pub trait TensorEmptyTrait<const DIM: usize>: TensorTrait<DIM>
 where
     Self::Data: DataEmptyTrait,
 {
@@ -28,14 +30,14 @@ where
 }
 
 /// A marker to create a tensor filled with a given value.
-pub trait TensorFullTrait: TensorTrait
+pub trait TensorFullTrait<const DIM: usize>: TensorTrait<DIM>
 where
     Self::Data: DataFullTrait,
     // ? It is possible to avoid this madness?
-    <<Self as TensorTrait>::Data as DataTrait>::Type: Copy + Zero + One,
+    <<Self as TensorTrait<DIM>>::Data as DataTrait>::Type: Copy + Zero + One,
 {
     /// Creates a data structure filled with `value`.
-    fn full(value: <<Self as TensorTrait>::Data as DataTrait>::Type) -> Self {
+    fn full(value: <<Self as TensorTrait<DIM>>::Data as DataTrait>::Type) -> Self {
         let data = Self::Data::full(value);
         unsafe { Self::new(data) }
     }
